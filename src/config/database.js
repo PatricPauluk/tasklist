d1cab6944b47325690a2202d255225a07ef4b78f
Patric Pauluk <patric.pauluk@outlook.com>
2022-10-19 00:00:10 -0300

    utilizando sequelize

---
+++ src/config/database.js
@@ -0,0 +1,27 @@
+// Dados de configuração do banco
+// Configuração de forma antiga para scan do sequelize
+
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgresql",
+  password: "",
+  database: "tasklist",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
+
+/* Explicações do define:
+
+timestamps:
+Cria campos created at e updated at para dados inseridos.
+
+underscored:
+Caso seja criado um model User, será criado uma tabela user_id.
+
+underscoredAll:
+Faz o mesmo mas não só com o nome da tabela, também com os relacionamentos.
+*/
