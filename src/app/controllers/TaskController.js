bdf38c32d1b7469688f287b2025eab4d67d8f20c
Patric Pauluk <patric.pauluk@outlook.com>
2022-11-04 19:27:47 -0300

    concluindo e deletando tasks marcadas

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -30,6 +30,56 @@ class TaskController {
 
     return res.json(tasks);
   }
+
+  // Finaliza a task
+  async update(req, res) {
+    /* Recebe o ID da task por parâmetro na URL
+    O parâmetro poderia ser referenciado como: req.params.task_id
+    */
+    const { task_id } = req.params;
+
+    // Busca a task no banco
+    const task = await Task.findByPk(task_id);
+
+    // Verifica se foi encontrada uma task
+    if (!task) {
+      return res.status(400).json({ error: "Tarefa não encontrada." });
+    }
+
+    // Verifica se o usuário é o autor da task para poder marca-la
+    if (task.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: "Você não pode marcar uma task que não é sua." });
+    }
+
+    // Atualiza conforme as informações passadas no body
+    await task.update(req.body);
+
+    return res.json(task);
+  }
+
+  // Deleta a task
+  async delete(req, res) {
+    const { task_id } = req.params;
+
+    const task = await Task.findByPk(task_id);
+
+    if (!task) {
+      return res.status(400).json({ error: "Tarefa não encontrada." });
+    }
+
+    if (task.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: "Você não pode deletar uma tarefa que não é sua." });
+    }
+
+    // Deleta a task
+    await task.destroy();
+
+    return res.send();
+  }
 }
 
 export default new TaskController();
