42a7fc8923e57ab691565ee260bee51259f3fef4
Patric Pauluk <patric.pauluk@outlook.com>
2022-11-04 21:22:47 -0300

    aplicando cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes";
 
 // Importando nossa database (o index não precisa ser especificado pois é chamado automaticamente).
@@ -13,6 +14,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
